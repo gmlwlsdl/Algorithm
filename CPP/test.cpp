39631000bfd425d006fcf36dd291d3528969d80c
gmlwlsdl <heejin094@naver.com>
2024-10-24 21:40:35 +0900

    add : 람다 함수에 대하여, 캡처에 대하여

--- CPP/test.cpp
+++ CPP/test.cpp
@@ -7,6 +7,9 @@ using namespace std;
 int main() {
     vector<int> numbers = {1, 2, 3, 4, 5};
 
+    // []에는 캡처할 변수를 넣음
+    // 캡처할 변수 != 파라미터, 해당 람다 함수의 외부에 있는 변수에 접근할 때 사용
+    // [&]는 모든 변수를 참조로 캡처한다는 뜻
     auto printNumber = [](int n) {
         cout << n << " ";
     };
@@ -17,7 +20,7 @@ int main() {
 
     cout << "Doubled Numbers: ";
     for_each(numbers.begin(), numbers.end(), [](int n) {
-        cout << n *2 << " ";
+        cout << n * 2 << " ";
     });
     cout << endl;
 
