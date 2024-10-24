b63caabf01257540f41b83985f68c054e33bc54e
gmlwlsdl <heejin094@naver.com>
2024-10-24 21:35:59 +0900

    add: 유니오니, for_each 학습

--- CPP/test.cpp
+++ CPP/test.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
-#include <unordered_set>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 int main() {
-    
+    vector<int> numbers = {1, 2, 3, 4, 5};
+
+    auto printNumber = [](int n) {
+        cout << n << " ";
+    };
+
+    cout << "Numbers: ";
+    for_each(numbers.begin(), numbers.end(), printNumber);
+    cout << endl;
+
+    cout << "Doubled Numbers: ";
+    for_each(numbers.begin(), numbers.end(), [](int n) {
+        cout << n *2 << " ";
+    });
+    cout << endl;
+
+    return 0;
 }
