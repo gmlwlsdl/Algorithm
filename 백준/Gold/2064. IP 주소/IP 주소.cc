f177a4856445be41120c155853ba821e7233fe0a
hazel <heejin094@naver.com>
2025-05-30 18:52:47 +0900

    [Gold IV] Title: IP 주소, Time: 0 ms, Memory: 2236 KB -BaekjoonHub

---
+++ 백준/Gold/2064. IP 주소/IP 주소.cc
@@ -0,0 +1,167 @@
+// ip 주소는 네 개의 바이트로 구성되어 있음
+// 각각을 10진수로 나타냄. 사이에 점을 찍어서 표현
+// 각 수는 0~255
+// 각각의 바이트를 8자리 이진수로 나타내고
+// 네 개 붙여놓은 32자리 이진수로 
+// IP 네트워크는 기본적으로 2의 M승개의 컴퓨터가 할당됨
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+vector<string> split(string input, char dlim) {
+    vector<string> result;
+    stringstream s;
+    string stringBuffer;
+    
+    s.str(input);
+
+    while (getline(s, stringBuffer, dlim)) {
+        result.push_back(stringBuffer);
+    }
+    
+    return result;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc;
+    cin >> tc;
+
+    // IP 입력받고
+    vector<string> s(tc);
+
+    for (int i = 0; i < tc; i++) {
+        cin >> s[i];
+    }
+
+    // . 기준으로 나눠서 2차원 벡터에 int로 저장
+    vector<vector<int>> v(tc);
+
+    for (int i = 0; i < s.size(); i++) {
+        vector<string> result = split(s[i], '.');
+
+        for (int j = 0; j < result.size(); j++) {
+            int t = stoi(result[j]);
+            v[i].push_back(t);
+        }
+    }
+
+    // 입력받은 주소가 1개일 경우
+    if (v.size() == 1) {
+        cout << s[0] << '\n' << "255.255.255.255";
+        return 0;
+    }
+
+    // 네트워크의 크기 구하기
+
+    // 1. 정렬해서 가장 큰 값, 작은 값 구하고
+    sort(v.begin(), v.end());
+
+    // 2. 각각의 주소값을 2진수로 변환해서 a[32], b[32]에 저장하고
+    int vv = v.size() - 1;
+    int a[32], b[32];
+
+    for (int i = 0; i < 4; i++) {
+        int aa = 128, bb = 128;
+
+        for (int j = 0; j < 8; j++) {
+            if (v[0][i] >= aa) {
+                a[j + i * 8] = 1;
+                v[0][i] -= aa;
+            } else {
+                a[j + i * 8] = 0;
+            }
+
+            aa /= 2;
+
+            if (v[vv][i] >= bb) {
+                b[j + i * 8] = 1;
+                v[vv][i] -= bb;
+            } else {
+                b[j + i * 8] = 0;
+            }
+
+            bb /= 2;
+        }
+    }
+
+    // 3. 두 배열을 비교해서 값이 달라지는 첫 번째 인덱스를 구하기
+    int index = 32;
+
+    for (int i = 0; i < 32; i++) {
+        if (a[i] != b[i]) {
+            index = i;
+            break;
+        }
+    }
+
+    // 해당 인덱스만큼 1, 그 이후엔 0으로 구성된 배열 만들기
+    int m[32];
+    
+    for (int i = index; i < 32; i++) {
+        m[i] = 0;
+    }
+
+    for (int i = 0; i < index; i++) {
+        m[i] = 1;
+    }
+
+    // 서브넷 마스크 값이 1이면 가장 작은 주소값, 아니면 0을 리턴
+    // 이게 주소의 최솟값 (시작값)이다.
+    int n[32];
+
+    for (int i = 0; i < 32; i++) {
+        if (m[i] == 1) {
+            n[i] = a[i];
+        } else {
+            n[i] = 0;
+        }
+    }
+
+    // 두 배열을 10진수로 변환
+    int mt[4], nt[4];
+
+    for (int i = 0; i < 4; i++) {
+        int k = 0, kk = 0, t = 128;
+
+        for (int j = 0; j < 8; j++) {
+            if (m[j + i * 8] == 1) {
+                k += t;
+            }
+
+            if (n[j + i * 8] == 1) {
+                kk += t;
+            }
+
+            t /= 2;
+        }
+
+        mt[i] = k;
+        nt[i] = kk;
+    }
+
+    // "."과 함께 출력
+    for (int i = 0; i < 4; i++) {
+        cout << nt[i];
+
+        if (i != 3) {
+            cout << '.';
+        }
+    }
+
+    cout << '\n';
+
+    for (int i = 0; i < 4; i++) {
+        cout << mt[i];
+        if (i != 3) {
+            cout << '.';
+        }
+    }
+}
