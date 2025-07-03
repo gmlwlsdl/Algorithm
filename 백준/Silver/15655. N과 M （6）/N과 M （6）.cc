a813b3754495e47e0d5985849ac4862f5b14e48b
hazel <heejin094@naver.com>
2025-07-03 20:07:09 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+vector<int> input;         // 중복 제거된 입력값
+vector<int> result;
+// vector<bool> visited;
+
+void func(int depth, int start) {
+    if (depth == m) {
+        for (int i = 0; i < m; i++) {
+            cout << result[i];
+			if (i != (m-1)) cout << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = start; i < input.size(); i++) {
+        result[depth] = input[i];
+        func(depth + 1, i + 1); // 다음엔 i + 1부터 시작 (오름차순 조합)
+    }
+}
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cin >> n >> m;
+
+    vector<int> temp(n);
+    for (int i = 0; i < n; i++) {
+        cin >> temp[i];
+    }
+
+    sort(temp.begin(), temp.end());
+
+	// 중복 제거
+    temp.erase(unique(temp.begin(), temp.end()), temp.end());
+
+    input = temp;
+
+    result.resize(m);
+    // visited.resize(input.size(), false);
+
+    func(0, 0);
+    return 0;
+}
