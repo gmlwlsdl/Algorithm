9c69fbd6599d7e848c7b18bf655441984a82e704
hazel <heejin094@naver.com>
2025-08-16 10:20:21 +0900

    [level 2] Title: 전화번호 목록, Time: 70.13 ms, Memory: 35.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -0,0 +1,21 @@
+// 중복되는 문자열이 있으면 false
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    bool answer = true;
+    
+    sort(phone_book.begin(), phone_book.end());
+    
+    for (int i = 0; i < phone_book.size() - 1; i++)
+    {
+        if (phone_book[i + 1].find(phone_book[i]) == 0)
+            return false; 
+    }
+    
+    return answer;
+}
